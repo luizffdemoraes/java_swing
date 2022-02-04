1f25079210cc8407e1d1e3d204bc1403a3ec9968
Luiz Felipe Moraes <65265977+Deehferreira@users.noreply.github.com>
2022-02-04 00:33:39 -0300

    Entendendo hierarquia de componentes

--- src/com/schoolofnet/Components.java
+++ src/com/schoolofnet/Components.java
@@ -1,6 +1,7 @@
 package com.schoolofnet;
 
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,27 +9,28 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class EventsSwing {
+public class Components {
 
 	private static JFrame mainFrame; // Janela
 	private static JPanel mainPanel;
 	private static JButton btnAction;
+	private static JTextField inputAction;
 
-	public EventsSwing() {
+	public Components() {
 		showGUI();
-
 	}
 
 	public static void main(String[] args) {
-		EventsSwing event = new EventsSwing();
+		EventsSwing newEvent = new EventsSwing();
 	}
 
 	private void showGUI() {
 		mainFrame = new JFrame();
-		mainFrame.setBounds(100, 100, 250, 200);
+		mainFrame.setBounds(100, 100, 400, 200);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainFrame.setVisible(true);
+		mainFrame.setLayout(new GridLayout(2, 0));
 
 		mainPanel = new JPanel(); // Um painel pode conter varios outros
 		mainPanel.setLayout(new FlowLayout());
@@ -37,10 +39,12 @@ public class EventsSwing {
 
 		btnAction = new JButton("Click Here");
 		btnAction.setActionCommand("Action");
+		inputAction = new JTextField(3000);
 
-		mainPanel.add(btnAction);
 		btnAction.addActionListener(new BtnHandler());
-
+		mainPanel.add(inputAction);
+		mainPanel.add(btnAction);
+		mainFrame.setVisible(true);
 	}
 
 	public class BtnHandler implements ActionListener {
@@ -52,11 +56,12 @@ public class EventsSwing {
 			switch (command) {
 			case "Action":
 				System.out.println("My first event");
-				JOptionPane.showMessageDialog(null, "My first event");
+				JOptionPane.showMessageDialog(null, inputAction.getText());
 				break;
 			default:
 				System.out.println("Invalid Action");
 			}
 		}
 	}
+
 }
