2aca43d7afb467a678858bc43aca48d2cbac68f8
Luiz Felipe Moraes <65265977+Deehferreira@users.noreply.github.com>
2022-02-04 03:01:36 -0300

    Refatorando Calc em MVC - Controller

--- src/com/schoolofnet/mvc/CalcView.java
+++ src/com/schoolofnet/mvc/CalcView.java
@@ -1,5 +1,47 @@
 package com.schoolofnet.mvc;
 
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
 public class CalcView {
 
+	private JFrame frame;
+	private JButton btnCalc;
+	private JTextField txtNum1;
+	private JTextField txtNum2;
+	private JComboBox op;
+
+	public CalcView() {
+		prepareGUI();
+	}
+
+	private void prepareGUI() {
+		frame = new JFrame("Calc");
+		frame.setBounds(100, 100, 400, 250);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new GridLayout(4,0));
+		
+		btnCalc = new JButton("Calcule");
+		txtNum1 = new JTextField(30);
+		txtNum2 = new JTextField(30);
+		op = new JComboBox<>();
+		
+		op.addItem("+");
+		op.addItem("-");
+		op.addItem("/");
+		op.addItem("*");
+		
+		frame.add(txtNum1);
+		frame.add(op);
+		frame.add(txtNum2);
+		frame.add(btnCalc);
+		
+		btnCalc.addActionListener(new CalcController(op, txtNum1, txtNum1));
+		
+		frame.setVisible(true);
+	}
 }
